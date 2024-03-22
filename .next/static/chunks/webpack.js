16f98922cdd3fb755f79cd6f87ce700b445a044e
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-03-22 21:24:25 +0500

    .next folder to .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44b4e6d855ddf1f5"; }
+/******/ 		__webpack_require__.h = function() { return "17784117f4afe1a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
